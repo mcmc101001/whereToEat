6ebc100ea8fd29723ec5baa1469054236d0b8c8c
mcmc101001 <tohmingchun101001@gmail.com>
2023-12-18 17:11:15 +0800

    Update view to fit in mobile form factor

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -4,7 +4,8 @@
   <main
     class="h-screen min-h-screen w-full bg-background text-foreground items-center justify-center flex flex-col"
   >
-  <div class="max-w-3xl items-center flex flex-col h-full relative">
-      <router-view /></div>
+    <div class="max-w-3xl items-center flex flex-col h-full relative">
+      <router-view />
+    </div>
   </main>
 </template>
