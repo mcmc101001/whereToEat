07b1524a41a80e0565d322c1fc2094cd8ed8864c
mcmc101001 <tohmingchun101001@gmail.com>
2023-12-18 20:51:15 +0800

    Change main page css to stop content covered when address bar is present

--- frontend/dev-dist/sw.js
+++ frontend/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-b5f7729d'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.50dds28vb0o"
+    "revision": "0.t2isiv99nk8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
