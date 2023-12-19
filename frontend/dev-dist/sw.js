3cddb9ecd31d95855f5983a83966192e59a55056
mcmc101001 <tohmingchun101001@gmail.com>
2023-12-19 17:37:46 +0800

    Modify theme for address bar

--- frontend/dev-dist/sw.js
+++ frontend/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-b5f7729d'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.df9rk1l6cf8"
+    "revision": "0.seruhn1t338"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
