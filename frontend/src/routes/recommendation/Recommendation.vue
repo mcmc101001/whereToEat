9f4edc1cd2f05108f96fd1f99d804c120887904a
mcmc101001 <tohmingchun101001@gmail.com>
2023-12-20 15:32:05 +0800

    Utilise KeepAlive vue component to prevent unnecessary photo fetches when backnavigating

--- frontend/src/routes/recommendation/Recommendation.vue
+++ frontend/src/routes/recommendation/Recommendation.vue
@@ -88,22 +88,24 @@ function prevPlace() {
     /></router-link>
     <div class="h-full w-full py-10 px-10 flex items-center justify-center flex-col gap-4">
       <div class="flex items-center justify-center flex-1">
-        <div
-          v-if="placeIndex === MAX_LOCATION_NUMBER || errorState"
-          class="text-primary flex flex-col gap-4 text-2xl p-6 h-full w-full items-center justify-center text-center"
-        >
-          <span v-if="errorState">
-            Something went wrong! Please try again later.
-          </span>
-          <span v-else-if="MAX_LOCATION_NUMBER === 20"
-            >Max locations reached! Try refining your filters!</span
+        <KeepAlive :max="MAX_LOCATION_NUMBER">
+          <div
+            v-if="placeIndex === MAX_LOCATION_NUMBER || errorState"
+            class="text-primary flex flex-col gap-4 text-2xl p-6 h-full w-full items-center justify-center text-center"
           >
-          <span v-else>No more locations found! Try modifying your filters!</span>
-          <router-link to="/" class="bg-primary rounded-full p-5 text-primary-foreground"
-            >Go back</router-link
-          >
-        </div>
-        <Card v-else :place="places[placeIndex]" :key="placeIndex" />
+            <span v-if="errorState">
+              Something went wrong! Please try again later.
+            </span>
+            <span v-else-if="MAX_LOCATION_NUMBER === 20"
+              >Max locations reached! Try refining your filters!</span
+            >
+            <span v-else>No more locations found! Try modifying your filters!</span>
+            <router-link to="/" class="bg-primary rounded-full p-5 text-primary-foreground"
+              >Go back</router-link
+            >
+          </div>
+          <Card v-else :place="places[placeIndex]" :key="placeIndex" />
+        </KeepAlive>
       </div>
       <div class="flex items-center justify-center w-full gap-3">
         <button
