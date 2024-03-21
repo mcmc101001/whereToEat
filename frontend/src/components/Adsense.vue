3a4b6cfe395a615c8c7d6456c03cb3998edddc0c
mcmc101001 <tohmingchun101001@gmail.com>
2024-03-21 23:54:57 +0800

    Fixed size for adsense

--- frontend/src/components/Adsense.vue
+++ frontend/src/components/Adsense.vue
@@ -11,8 +11,8 @@ onMounted(() => {
 <template>
   <ins
     class="adsbygoogle"
-    style="display: block"
+    style="display: inline-block; width: 320px; height: 80px"
     data-ad-client="ca-pub-4769257199482076"
-    data-ad-slot="1639459037"
+    data-ad-slot="1731808433"
   ></ins>
 </template>
